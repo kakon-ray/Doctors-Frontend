c78cc125000036e0d8acb1f37e573fb897002154
Kakon Ray <thisiskakonray@gmail.com>
2022-05-20 18:08:13 +0600

    completed

---
+++ src/Pages/Dashboard/CheckoutForm.js
@@ -0,0 +1,119 @@
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import React, { useEffect, useState } from "react";
+
+const CheckoutForm = ({ data }) => {
+  const [cardError, setCardError] = useState("");
+  const [success, setSuccess] = useState("");
+  const [transactionId, setTransactionId] = useState("");
+
+  const stripe = useStripe();
+  const elements = useElements();
+  const [clientSecret, setClientSecret] = useState("");
+
+  const { price, name, email } = data;
+
+  useEffect(() => {
+    fetch("http://localhost:5000/create-payment-intent", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+      body: JSON.stringify({ price }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data?.clientSecret) {
+          setClientSecret(data.clientSecret);
+        }
+      });
+  }, [price]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+
+    const card = elements.getElement(CardElement);
+
+    if (card === null) {
+      return;
+    }
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card: elements.getElement(CardElement),
+    });
+
+    if (error) {
+      setCardError(error.message);
+      setSuccess("");
+    } else {
+      setCardError("");
+    }
+
+    // Confirm card pement
+
+    const { paymentIntent, error: intentError } =
+      await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: card,
+          billing_details: {
+            name: name,
+            email: email,
+          },
+        },
+      });
+
+    if (intentError) {
+      setCardError(intentError.message);
+      setSuccess("");
+    } else {
+      setCardError("");
+
+      setTransactionId(paymentIntent.id);
+      setSuccess("Congrets! Your Pement is completed");
+    }
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: "16px",
+                color: "#424770",
+                "::placeholder": {
+                  color: "#aab7c4",
+                },
+              },
+              invalid: {
+                color: "#9e2146",
+              },
+            },
+          }}
+        />
+        <button
+          className="btn btn-success btn-sm"
+          type="submit"
+          disabled={!stripe || !clientSecret}
+        >
+          Pay
+        </button>
+      </form>
+      {cardError && <p className="text-red-500">{cardError}</p>}
+
+      {success && (
+        <div className="text-green-500">
+          <p className="font-bold">{success} </p>
+          <p className="text-orange-600">{transactionId} </p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CheckoutForm;
--- src/Pages/Dashboard/Payment.js
+++ src/Pages/Dashboard/Payment.js
@@ -1,8 +1,28 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 import { signOut } from "firebase/auth";
 import React, { useEffect } from "react";
 import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import Loading from "../Shared/Loading";
+import CheckoutForm from "./CheckoutForm";
+
+/**
+ * 1. install stripe react stripe js
+ * 2. open stripe account on stripe website
+ * 3. get publishable key pk___
+ * 4. Create Elements wrapper using publishable key
+ * 5. Create Checkout Form using Card element, useStripe, useElements
+ * 6. get card elements info(credit card info)
+ * 7. get credit card info/error + display card error (if any)
+ * ----------------
+ * 9. get client secret from backend via payment intent post api
+ * 10. store client secret on the client side
+ * 11. Create card payment confirmation
+ * 12. once payment is confirmed save payment information to the server
+ * 13. display transaction id
+ * pement getway completed
+ */
 
 const Payment = () => {
   const { id } = useParams();
@@ -20,17 +40,25 @@ const Payment = () => {
     return <Loading />;
   }
 
+  const stripePromise = loadStripe(
+    "pk_test_51L1Q4WAIzqv6QO7WbF65f9XiKcZCgtmjJKOV0xZ2FiSu9E0SUzSv4Oww4ypuScqtuG0xEw38Rm3izV86U3GzCpA700fOuyGypr"
+  );
+
   return (
-    <div>
-      <div class="card w-96 bg-base-100 shadow-xl">
+    <div className="flex">
+      <div class="card w-96 bg-base-100 shadow-xl m-6">
         <div class="card-body">
-          <h2 class="card-title">{data.tretmentName}</h2>
-          <p>{data.name}</p>
-          <p>{data.date}</p>
-          <p>Please Pay for ${data.price}</p>
-          <div class="card-actions justify-end">
-            <button class="btn btn-sm">Pay Now</button>
-          </div>
+          <h2 class="card-title">{data?.tretmentName}</h2>
+          <p>{data?.name}</p>
+          <p>{data?.date}</p>
+          <p>Please Pay for ${data?.price}</p>
+        </div>
+      </div>
+      <div class="card w-96 bg-base-100 shadow-xl m-6">
+        <div class="card-body">
+          <Elements stripe={stripePromise}>
+            <CheckoutForm data={data} />
+          </Elements>
         </div>
       </div>
     </div>
